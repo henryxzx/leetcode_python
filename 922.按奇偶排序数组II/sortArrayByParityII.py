129079c16d809a57afad59bbd632bb667d59ab15
XZX <35793964+henryxzx@users.noreply.github.com>
2018-11-18 00:29:42 +0800

    解得很烂

--- 922.按奇偶排序数组II/sortArrayByParityII.py
+++ 922.按奇偶排序数组II/sortArrayByParityII.py
@@ -4,7 +4,14 @@ class Solution:
         :type A: List[int]
         :rtype: List[int]
         """
-        ans = []
+        even, odd, ans = [], [], []
+        for i in A:
+            if i % 2 == 0:
+                even.append(i)
+            else:
+                odd.append(i)
         for i in range(len(A)):
             if i % 2 == 0:
-               ans.append()
+                ans.append(even.pop())
+            else:
+                ans.append(odd.pop())
