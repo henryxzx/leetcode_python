0022aaa979c9310d977a9de692a83eb59dbe0a8f
XZX <35793964+henryxzx@users.noreply.github.com>
2018-11-17 23:53:03 +0800

    Add files via upload

---
+++ 922.按奇偶排序数组II/sortArrayByParityII.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sortArrayByParityII(self, A):
+        """
+        :type A: List[int]
+        :rtype: List[int]
+        """
+        ans = []
+        for i in range(len(A)):
+            if i % 2 == 0:
+               ans.append()
